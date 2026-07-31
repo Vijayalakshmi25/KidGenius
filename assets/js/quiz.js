7d355fca5463caede72bcc7bede12a1029e389cd
Vijaya Lakshmi Mandalla <34175632+Vijayalakshmi25@users.noreply.github.com>
2026-07-31 18:36:17 -0400

    Create quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,174 @@
+let questions;
+
+
+let index=0;
+
+
+let score=0;
+
+
+
+fetch("../data/questions/math.json")
+
+.then(res=>res.json())
+
+.then(data=>{
+
+
+let quiz=
+localStorage.getItem(
+"currentQuiz"
+);
+
+
+questions=data[quiz];
+
+
+showQuestion();
+
+
+});
+
+
+
+
+
+function showQuestion(){
+
+
+let q =
+questions[index];
+
+
+
+document.getElementById(
+"quiz"
+).innerHTML=
+
+
+`
+
+<h2>
+${q.question}
+</h2>
+
+
+${q.options.map(
+
+x=>`
+
+<button onclick="
+checkAnswer(${x})
+">
+
+${x}
+
+</button>
+
+`
+
+).join("")}
+
+`;
+
+
+
+}
+
+
+
+
+function checkAnswer(answer){
+
+
+if(answer==
+questions[index].answer){
+
+score++;
+
+}
+
+
+
+index++;
+
+
+
+if(index < questions.length){
+
+showQuestion();
+
+}
+
+else{
+
+finishQuiz();
+
+}
+
+
+}
+
+
+
+
+function finishQuiz(){
+
+
+let xp =
+score*10;
+
+
+let player =
+JSON.parse(
+localStorage.getItem(
+"kidgenius_player"
+)
+);
+
+
+
+player.xp += xp;
+
+player.stars += score;
+
+
+localStorage.setItem(
+
+"kidgenius_player",
+
+JSON.stringify(player)
+
+);
+
+
+
+document.getElementById(
+"quiz"
+).innerHTML=
+
+`
+
+<h2>
+🎉 Completed
+</h2>
+
+<h3>
+Score:
+${score}/${questions.length}
+</h3>
+
+
+<p>
++${xp} XP earned
+</p>
+
+<a href="../index.html">
+
+Back Home
+
+</a>
+
+`;
+
+}
